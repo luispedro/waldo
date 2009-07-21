71d313804f5dd69c424067dca18f93a59991170d
Luís Pedro Coelho <lpc@cmu.edu>
2009-07-21 11:04:28 -0400

    Call Synergizer to translate IDs
    Right now, only supports ensembl gene to ensembl peptide

---
+++ tests/test_synergizer.py
@@ -0,0 +1,13 @@
+import protloc.synergizer
+def test_positive():
+    key = 'ENSMUSG00000034254'
+    trans = protloc.synergizer.translate_ensembl_gene_to_ensembl_peptide([key])
+    assert key in trans
+    assert trans[key]
+
+def test_negative():
+    key = 'this_is_not_a_key'
+    trans = protloc.synergizer.translate_ensembl_gene_to_ensembl_peptide([key])
+    assert key in trans
+    assert trans[key] is None
+
