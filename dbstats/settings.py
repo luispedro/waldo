7f36a911fa22fa2b2e96e9024bdbcdd2da40007c
Shannon <squinn@vrononlnx.(none)>
2010-01-26 13:22:08 -0500

    Changed structure slightly. More changes forthcoming.

---
+++ dbstats/settings.py
@@ -0,0 +1,15 @@
+"""
+This file contains all the settings needed to run the Waldo application.
+"""
+
+# this is the system path to all the flat database files
+DATABASE_FILE_FOLDER = '../../databases/'
+
+# Database setup
+DB_USERNAME = 'researchuser'
+DB_PASSWORD = 'r3NMYGccy7PLQp9b'
+DB_NAME = 'waldo'
+DB_HOST = 'localhost'
+
+# debug mode
+DEBUG = True
