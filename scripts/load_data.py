a511b10ae469706cc8695d808c8af41ab2eb0af6
Luis Pedro Coelho <lpc@cmu.edu>
2010-01-29 15:43:14 -0500

    Add header to load_data.py

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009-2010, Luis Pedro Coelho <lpc@cmu.edu>
+# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
+# License: MIT. See COPYING.MIT file in the Waldo distribution
+#
 import backend
 import mgi.files
 import mgi.load
