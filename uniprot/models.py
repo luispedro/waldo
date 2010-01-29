d4bf287976b2600ea22a704dd857c2caf935792f
Luis Pedro Coelho <lpc@cmu.edu>
2010-01-29 15:43:33 -0500

    Remove unnecessary import

--- uniprot/models.py
+++ uniprot/models.py
@@ -23,7 +23,6 @@
 from __future__ import division
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relation, backref
-from sqlalchemy.ext.declarative import declarative_base
 from backend import Base
 
 class Accession(Base):
