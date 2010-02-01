43e054b681fdce98943164f4bcdecf368c8bbde9
Luis Pedro Coelho <lpc@cmu.edu>
2010-02-01 11:47:40 -0500

    Display uniprot results

--- woof/odlaw/views.py
+++ woof/odlaw/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
+import uniprot.retrieve
 
 def searchby(request):
     if request.method == 'GET':
@@ -7,9 +8,13 @@ def searchby(request):
         return HttpResponseRedirect('/search/ensemblid/' + ensemblid)
 
 def search(request, ensemblid):
+    uniprot_name = uniprot.retrieve.from_ensembl_gene_id(ensemblid)
+    go_terms = uniprot.retrieve.retrieve_go_annotations(uniprot_name)
     return render_to_response(
                 'results.html',
                 {
                     'genename' : ensemblid,
+                    'uniprot_name' : uniprot_name,
+                    'uniprot_goterms' : go_terms,
                 })
                  
