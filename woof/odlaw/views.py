b02811b06d2e86349c9a3cfed1a89c6a2145035b
Luis Pedro Coelho <lpc@cmu.edu>
2010-02-01 12:37:47 -0500

    Display MGI information

--- woof/odlaw/views.py
+++ woof/odlaw/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 import uniprot.retrieve
+import mgi.retrieve
 
 def searchby(request):
     if request.method == 'GET':
@@ -9,12 +10,17 @@ def searchby(request):
 
 def search(request, ensemblid):
     uniprot_name = uniprot.retrieve.from_ensembl_gene_id(ensemblid)
-    go_terms = uniprot.retrieve.retrieve_go_annotations(uniprot_name)
+    uniprot_go_terms = uniprot.retrieve.retrieve_go_annotations(uniprot_name)
+
+    mgi_id = mgi.retrieve.from_ensembl_gene_id(ensemblid)
+    mgi_go_terms = mgi.retrieve.retrieve_go_annotations(mgi_id)
     return render_to_response(
                 'results.html',
                 {
                     'genename' : ensemblid,
                     'uniprot_name' : uniprot_name,
-                    'uniprot_goterms' : go_terms,
+                    'uniprot_go_terms' : uniprot_go_terms,
+                    'mgi_id' : mgi_id,
+                    'mgi_go_terms' : mgi_go_terms,
                 })
                  
