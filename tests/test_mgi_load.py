a3b31c0a3157accb76611d57caa591cd2cdf8d7a
Luis Pedro Coelho <lpc@cmu.edu>
2010-01-31 23:48:31 -0500

    Test MGI translation loading too.

--- tests/test_mgi_load.py
+++ tests/test_mgi_load.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import and_
 
 import mgi
 import mgi.load
 import mgi.models
+from translations.models import Translation
 
 def test_mgi_load():
     engine = create_engine('sqlite://')
@@ -21,4 +23,10 @@ def test_mgi_load():
     assert loaded == nr_entries
     # MGI:1915545 is in the file but does not have a cellular component
     assert not session.query(mgi.models.Entry).filter(mgi.models.Entry.mgi_id == 'MGI:1915545').count()
+
+    assert session.query(Translation).filter(
+                        and_(Translation.input_namespace == 'ensembl:gene_id',
+                            Translation.input_name == 'ENSMUSG00000026004',
+                            Translation.output_namespace ==  'mgi')).count()
+
 
