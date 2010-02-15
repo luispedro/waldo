988b891c4c71086bfd98afae644d2fe7e25bea67
Luis Pedro Coelho <lpc@cmu.edu>
2010-02-15 15:18:35 -0500

    Remove duplicate code

--- waldo/uniprot/online.py
+++ waldo/uniprot/online.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2009, Luís Pedro Coelho <lpc@cmu.edu>
+# Copyright (C) 2009-2010, Luis Pedro Coelho <lpc@cmu.edu>
+# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 #  of this software and associated documentation files (the "Software"), to deal
@@ -20,37 +21,49 @@
 #  THE SOFTWARE.
 
 from __future__ import division
-import numpy as np
+from collections import defaultdict
 import urllib
 import re
-import waldo.util.proteinlocations
 
-__all__ = ['MUS_MUSCULUS','locate']
+__all__ = ['MUS_MUSCULUS','query']
 
 MUS_MUSCULUS = 'Mus Musculus'
-def locate(ensemblgeneid):
-	'''
-	locations = locate(ensemblgeneid):
+def query(name=None, ensembl=None, gene_name=None, organism=None):
+    '''
+    locations = query(name=None, ensembl=None, gene_name=None, organism=None):
 
-	Uniprot location for protein, identified by:
-	* ensemblgeneid: Ensembl gene ID
-	'''
+    Uniprot location for protein, identified by one of:
+        - name: Gene or protein name (free-text search)
+        - gene_name: Gene name
+        - ensembl: Ensembl gene or peptide ID
+        - organism: Organism (e.g., 'Mus Musculus' or 'Mouse')
+    '''
+    # This is based on a Matlab implementation of the same idea by Yanhua
 
-	# This is based on the Uniprot locate code by Luis Pedro Coelho
-	query = 'reviewed:yes'
-        query += ' ensembl %s' % ensemblgeneid
-	accessions = []
-	url = 'http://www.uniprot.org/uniprot/?' + urllib.urlencode({'query' : query, 'format':'tab', 'columns':'id'})
-	for line in urllib.urlopen(url):
-		accessions.append(line.strip())
-	results = util.proteinlocations.ProteinLocations()
-	for acc in accessions:
-		url = 'http://www.uniprot.org/uniprot/'
-		for line in urllib.urlopen(url + '%s.txt' % acc):
-			match = re.search('GO; +GO:([0-9]+);\s*C:([^;]*);',line)
-			if match:
-				go_nr,go_name = match.groups()
-				results.addElement(go_name, go_nr, url + acc)
-	return results
+    query = 'reviewed:yes'
+    if name:
+        query += ' ' + name
+    if gene_name:
+        query = ' gene:' + gene_name
+    if organism:
+        query += ' organism:"%s"' % organism
+    if ensembl:
+        query += ' ensembl %s' % ensembl
+    if not query:
+        raise ValueError, 'Uniprot.online.query: Empty query'
+    accessions = []
+    url = 'http://www.uniprot.org/uniprot/?' + urllib.urlencode({'query' : query, 'format':'tab', 'columns':'id'})
+    for line in urllib.urlopen(url):
+        accessions.append(line.strip())
+    results = defaultdict(int)
+    for acc in accessions:
+        for line in urllib.urlopen('http://www.uniprot.org/uniprot/%s.txt' % acc):
+            match = re.search('GO; +GO:([0-9]+);\s*C:([^;]*);',line)
+            if match:
+                go_nr,go_name = match.groups()
+                results[go_name] += 1
+    return dict(results)
 
-# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
+if __name__ == "__main__":
+    print query(ensembl='ENSMUSG00000034254')
+
