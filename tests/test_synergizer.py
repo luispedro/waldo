b5249dd6c889bb0c20bb9ca447ae8dc62be9eada
Luis Pedro Coelho <lpc@cmu.edu>
2010-02-16 22:34:15 -0500

    Move synergizer one level up

--- tests/test_synergizer.py
+++ tests/test_synergizer.py
@@ -1,13 +1,13 @@
-import waldo.protloc.synergizer
+import waldo.synergizer
 def test_positive():
     key = 'ENSMUSG00000034254'
-    trans = waldo.protloc.synergizer.translate_ensembl_gene_to_ensembl_peptide([key])
+    trans = waldo.synergizer.translate_ensembl_gene_to_ensembl_peptide([key])
     assert key in trans
     assert trans[key]
 
 def test_negative():
     key = 'this_is_not_a_key'
-    trans = waldo.protloc.synergizer.translate_ensembl_gene_to_ensembl_peptide([key])
+    trans = waldo.synergizer.translate_ensembl_gene_to_ensembl_peptide([key])
     assert key in trans
     assert trans[key] is None
 
