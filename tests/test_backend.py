1619cab66aa226b2792368aee0fba2b7371f356f
Luis Pedro Coelho <lpc@cmu.edu>
2010-02-22 01:48:17 -0500

    Smoke test for create_tables()

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,6 +1,12 @@
+from sqlalchemy import create_engine
 import waldo.backend
 
 def test_backend():
     assert waldo.backend.Base
     assert waldo.backend.engine
+
+def test_create_all():
+    metadata = waldo.backend.metadata
+    metadata.bind = create_engine('sqlite://')
+    metadata.create_all()
 
