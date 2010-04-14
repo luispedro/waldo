01992bd20b928b434746c157a7f93fbcca643104
Luis Pedro Coelho <lpc@cmu.edu>
2010-04-14 14:52:23 -0400

    Simplify code.
    Change output format to always use '\n' instead of '\n' for stdout and
    '\r\n' for file output.

--- batch.py
+++ batch.py
@@ -6,41 +6,37 @@
 import sys
 
 import waldo.backend
-from waldo import mgi
-from waldo import uniprot
-from waldo import esldb
-from waldo import locate
-from waldo import hpa
 from waldo.groupings import Grouping
 
-if len(sys.argv) < 2:
-    quit('Please provide a path to a file containing newline-delimited protein IDs.\n' + \
-        '\tpython batch.py <inputfile> [outputfile]')
+session = waldo.backend.create_session()
 
-output = None
-format = "ensemblid,locate_locations,locate_link,mgi_locations,mgi_link,esldb_locations,esldb_link,uniprot_locations,uniprot_link,hpa_locations,hpa_link"
+if len(sys.argv) < 2:
+    input = sys.stdin
+else:
+    input = file(sys.argv[1])
 
 if len(sys.argv) == 3:
     try:
         output = open(sys.argv[2], "w")
-    except IOError:
-        quit('When specifying an output file, please make sure the directory and ' + \
-            'file is writeable.')
+    except IOError, e:
+        print >>sys.stderr, '''
+Error opening '%s': %s
 
-    # print the format for the output
-    output.write(format + '\r\n')
+When specifying an output file, please make sure the directory and file is writeable.''' % (sys.argv[2], e)
+        sys.exit(1)
 else:
-    print format
-    
-session = waldo.backend.create_session()
-for id in file(sys.argv[1]):
-    if len(id) <= 1: continue
-    # process each ID
-    ids = Grouping(id.strip(), session)
-    if output is None:
-        print ids.getBatch()
-    else:
-        output.write(ids.getBatch() + '\r\n')
+    output = sys.stdout
 
-if output is not None:
+
+format = "ensemblid,locate_locations,locate_link,mgi_locations,mgi_link,esldb_locations,esldb_link,uniprot_locations,uniprot_link,hpa_locations,hpa_link\n"
+output.write(format)
+for id in input:
+    id = id.strip()
+    if not id:
+        continue
+    ids = Grouping(id.strip(), session)
+    output.write(ids.getBatch() + '\n')
+
+if output is not sys.stdout:
     output.close()
+
