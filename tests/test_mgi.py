3395c1b2088a16bab3394bef25fcf4b9f55f3202
Luis Pedro Coelho <lpc@cmu.edu>
2010-05-06 14:37:16 -0400

    Rework mgi tests to work on the test database
    Ideally, tests should run on the tests/data only.

--- tests/test_mgi.py
+++ tests/test_mgi.py
@@ -1,8 +1,42 @@
-import waldo.backend
-import waldo.mgi.models
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import and_
 
-session = waldo.backend.create_session()
+import waldo.mgi
+import waldo.mgi.load
+import waldo.mgi.models
+from waldo.translations.models import Translation
+
+def load_mgi_test():
+    engine = create_engine('sqlite://')
+    metadata = waldo.mgi.models.Base.metadata
+    metadata.bind = engine
+    metadata.create_all()
+    sessionmaker_ = sessionmaker(engine)
+    _testinput = 'tests/data/'
+
+    nr_entries = waldo.mgi.load.load(_testinput, sessionmaker_)
+    session = sessionmaker_ ()
+    return session, nr_entries
+
+def test_mgi_load():
+    session, nr_entries = load_mgi_test()
+    loaded = session.query(waldo.mgi.models.Entry).count()
+
+    assert nr_entries == 15
+    assert loaded == nr_entries
+    # MGI:1915545 is in the file but does not have a cellular component
+    assert not session.query(waldo.mgi.models.Entry).filter(waldo.mgi.models.Entry.mgi_id == 'MGI:1915545').count()
+
+    for namespace in ('mgi:name', 'mgi:id', 'mgi:symbol'):
+        assert session.query(Translation).filter(
+                            and_(Translation.input_namespace == 'ensembl:gene_id',
+                                Translation.input_name == 'ENSMUSG00000026004',
+                                Translation.output_namespace == namespace)).count()
+
+
 def test_mgi():
+    session,_ = load_mgi_test()
     def test_annotated(id, go_id, tf):
         first = session.query(waldo.mgi.models.Entry).filter(waldo.mgi.models.Entry.mgi_id == id).first()
         st = go_id in map(lambda ann: ann.go_id, first.annotations)
