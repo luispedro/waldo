120282d4d4642757d67502c07574c08f84abd2b7
Luis Pedro Coelho <lpc@cmu.edu>
2010-05-06 15:06:45 -0400

    Make GO test be all in memory

--- tests/test_go.py
+++ tests/test_go.py
@@ -10,13 +10,13 @@ def test_is_cellular_component():
     metadata.bind = engine
     metadata.create_all()
     sessionmaker_ = sessionmaker(engine)
+    session = sessionmaker_ ()
 
     nr_entries = waldo.go.load.load('tests/data/', sessionmaker_)
     expected = 976 - 79 # instances of "[Term]" - instances of is_obsolete
     assert nr_entries == expected
 
-    session = sessionmaker_ ()
-    assert session.query( waldo.go.models.Term).count() == nr_entries
+    assert session.query(waldo.go.models.Term).count() == nr_entries
 
     assert waldo.go.go.is_cellular_component('GO:0000015')
     assert waldo.go.go.is_cellular_component('GO:0000108')
@@ -26,23 +26,17 @@ def test_is_cellular_component():
     assert waldo.go.go.is_cellular_component('GO:0000108')
     assert not waldo.go.go.is_cellular_component('GO:0000107')
 
-def test_id_to_term():
-    engine = create_engine('sqlite://')
-    metadata = waldo.go.models.Base.metadata
-    metadata.bind = engine
-    metadata.create_all()
-    sessionmaker_ = sessionmaker(engine)
-    
-    waldo.go.load.load('tests/data/', sessionmaker_)
-    session = sessionmaker_()
+
+
 
     id = 'GO:0000015'
     term = 'phosphopyruvate hydratase complex'
-    result = waldo.go.go.id_to_term(id)
+    result = waldo.go.go.id_to_term(id, session)
     assert result is not None
     assert result == term
 
     id = 'GO:00099999skl'
     term = 'repairosome'
-    result = waldo.go.go.id_to_term(id) 
+    result = waldo.go.go.id_to_term(id, session)
     assert result == id
+
