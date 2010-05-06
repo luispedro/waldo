a6e7324916625346b11edd2fb596bbee5efbd1de
Luis Pedro Coelho <lpc@cmu.edu>
2010-05-06 14:26:26 -0400

    Test for correct exception

--- tests/test_translation.py
+++ tests/test_translation.py
@@ -5,7 +5,7 @@ from nose.tools import raises
 def test_verify_namespace_ok():
     waldo.translations.models.verify_namespace('ensembl:gene_id')
 
-@raises(AssertionError)
+@raises(ValueError)
 def test_verify_namespace_fail():
     waldo.translations.models.verify_namespace('ensembl:gene_idasas')
 
