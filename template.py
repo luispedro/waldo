071eab0e392bc28797b8b5cfeb7c896410a6f84f
Luis Pedro Coelho <luis@luispedro.org>
2012-01-18 16:46:27 +0000

    RFCT Remove unused file

--- template.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2009-2010, Luis Pedro Coelho <luis@luispedro.org>
-# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
-# License: MIT. See COPYING.MIT file in the Waldo distribution
-
-from __future__ import division
-import numpy as np
-
