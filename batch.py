67913ae7b847e8d3899c1c9c22409e540dbb2c56
Luis Pedro Coelho <luis@luispedro.org>
2012-01-18 16:33:11 +0000

    ENH Add --help handling
    Also, refactor the code

--- batch.py
+++ batch.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2009-2010, Shannon Quinn <squinn@cmu.edu>, Luis Pedro Coelho <luis@luispedro.org>
+# Copyright (C) 2009-2012
+# Shannon Quinn <squinn@cmu.edu>
+# Luis Pedro Coelho <luis@luispedro.org>
 # vim: set ts=4 sts=4 sw=4 expandtab smartindent:
 # License: MIT. See COPYING.MIT file in the Waldo distribution
 
@@ -8,35 +10,48 @@ import sys
 import waldo.backend
 from waldo.retrieve_all import get_information
 
-session = waldo.backend.create_session()
 
-if len(sys.argv) < 2:
+def print_info(input, output):
+    session = waldo.backend.create_session()
+    format = "ensemblid,locate_locations,locate_link,mgi_locations,mgi_link,hpa_locations,hpa_link,uniprot_locations,uniprot_link\n"
+    output.write(format)
+    for id in input:
+        id = id.strip()
+        if not id:
+            continue
+        output.write(get_information(id, session))
+        output.write('\n')
+
+    if output is not sys.stdout:
+        output.close()
+
+def usage():
+    print '''
+waldo [INPUTFILE] [OUTPUTFILE]
+
+Reads a list of Ensembl Peptide IDs and outputs a CSV file with their database
+locations.
+'''
+
+def main(argv):
+    if len(argv) == 1 and argv[1] in ('-h', '--help'):
+        usage()
+        return
     input = sys.stdin
-else:
-    input = file(sys.argv[1])
-
-if len(sys.argv) == 3:
-    try:
-        output = open(sys.argv[2], "w")
-    except IOError, e:
-        print >>sys.stderr, '''
-Error opening '%s': %s
-
-When specifying an output file, please make sure the directory and file are writeable.''' % (sys.argv[2], e)
-        sys.exit(1)
-else:
     output = sys.stdout
+    if len(argv) > 1:
+        input = file(argv[1])
+    if len(argv) == 3:
+        try:
+            output = open(argv[2], "w")
+        except IOError, e:
+            print >>sys.stderr, '''
+waldo: Error opening '%s': %s
+
+When specifying an output file, please make sure the directory and file are writeable.''' % (argv[2], e)
+            sys.exit(1)
+    print_info(input, output)
 
 
-format = "ensemblid,locate_locations,locate_link,mgi_locations,mgi_link,hpa_locations,hpa_link,uniprot_locations,uniprot_link\n"
-output.write(format)
-for id in input:
-    id = id.strip()
-    if not id:
-        continue
-    output.write(get_information(id, session))
-    output.write('\n')
-
-if output is not sys.stdout:
-    output.close()
-
+if __name__ == '__main__':
+    main(sys.argv)
