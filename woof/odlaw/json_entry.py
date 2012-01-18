29a66a1b74b4762b9e3f44f88df80900339fb7a6
Luis Pedro Coelho <luis@luispedro.org>
2012-01-18 16:14:00 +0000

    RFCT Clean up code

--- woof/odlaw/json_entry.py
+++ woof/odlaw/json_entry.py
@@ -6,21 +6,17 @@ class EntryEncoder(json.JSONEncoder):
 
     def default(self, entry):
         '''
-            Introspectively generates a json object from a database entry
-        '''
-        json_set = []
-        
-        for a in dir(entry) :
-            if not (a.startswith("_")) :
-                val = getattr(entry, a)
-                if type(val) is unicode or type(val) is int:
-                    json_set.append((a, val))
-                elif type(val) is sqlalchemy.orm.collections.InstrumentedList :
-                    #recurse on relations
-                    subset = []
-                    for item in val:
-                        subset.append(self.default(item))
-                    json_set.append((a, subset))
+        json = E.default(entry)
 
-        json_set = dict(json_set)
-        return json_set
+        Introspectively generates a json object from a database entry
+        '''
+        res = {}
+        for name in dir(entry) :
+            if name.startswith('_'):
+                continue
+            val = getattr(entry, name)
+            if isinstance(val, (unicode, str, int)):
+                res[name] = val
+            elif isinstance(val, InstrumentedList):
+                res[name] = map(self.default, val)
+        return res
