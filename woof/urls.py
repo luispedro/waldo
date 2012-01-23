fbf6d9e003b78fdb002abf0b809b8dceb1a967b4
Luis Pedro Coelho <luis@luispedro.org>
2012-01-23 16:32:52 +0000

    ENH Place holders for static content

--- woof/urls.py
+++ woof/urls.py
@@ -14,5 +14,8 @@ urlpatterns = patterns('',
     (r'^json/', include('woof.odlaw.urls')),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
+    (r'^help$', direct_to_template, {'template' : 'static/help.html'}, 'help'),
+    (r'^about$', direct_to_template, {'template' : 'static/about.html'}, 'about'),
+    (r'^contact-us$', direct_to_template, {'template' : 'static/contact-us.html'}, 'contact-us'),
     (r'^$', direct_to_template, {'template' : 'index.html'}, 'home'),
 )
