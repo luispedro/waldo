cce25a5f7222c2a1fcbba1bf12ab503833e7b3f8
Luis Pedro Coelho <luis@luispedro.org>
2012-06-28 19:35:44 +0100

    TST Break up tests

--- tests/test_uniprot_load.py
+++ tests/test_uniprot_load.py
@@ -44,15 +44,6 @@ def test_nr_entries():
     session,loaded = load_uniprot_test()
     assert loaded == (nr_entries + nr_names)
     test_entry = waldo.uniprot.retrieve.retrieve_entry('2AAA_MOUSE', session)
-    pubmed_test = "PMID: 17239238 [PubMed - indexed for MEDLINE]"
-    doi_test = "PMID: 15489334 [PubMed - indexed for MEDLINE]"
-
-    pubmed_abs = waldo.uniprot.retrieve.retrieve_pubmed_abstract("17239238")
-    assert pubmed_abs.endswith(pubmed_test)
-    doi_abs = waldo.uniprot.retrieve.retrieve_doi_abstract("10.1101/gr.2596504")
-    assert doi_abs.endswith(doi_test)
-    assert None == waldo.uniprot.retrieve.retrieve_doi_abstract("not a doi code")
-
     assert session.query(Translation).filter(and_(Translation.input_namespace == 'ensembl:gene_id', Translation.output_namespace ==  'uniprot:name')).count()
     assert waldo.uniprot.retrieve.from_ensembl_gene_id('ENSMUSG00000047281', session) == '1433S_MOUSE'
     assert 'GO:0000159' in waldo.uniprot.retrieve.retrieve_go_annotations('2AAA_MOUSE', session)
@@ -63,6 +54,16 @@ def test_nr_entries():
         entries = waldo.uniprot.retrieve.retrieve_name_matches('subunit', session)
         for ent in entries:
             assert ent.rname.find("subunit") != -1
+
+def test_retrieve_pubmed():
+    pubmed_test = "PMID: 17239238 [PubMed - indexed for MEDLINE]"
+    doi_test = "PMID: 15489334 [PubMed - indexed for MEDLINE]"
+
+    pubmed_abs = waldo.uniprot.retrieve.retrieve_pubmed_abstract("17239238")
+    assert pubmed_abs.endswith(pubmed_test)
+    doi_abs = waldo.uniprot.retrieve.retrieve_doi_abstract("10.1101/gr.2596504")
+    assert doi_abs.endswith(doi_test)
+    assert None == waldo.uniprot.retrieve.retrieve_doi_abstract("not a doi code")
 
 
 def test_clear():
