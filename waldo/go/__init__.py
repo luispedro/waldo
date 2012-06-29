2246f307d1444414c9e2dc1913fd7de3212d46ef
Luis Pedro Coelho <luis@luispedro.org>
2012-06-29 17:35:31 +0100

    ENH Export names at higher level

--- waldo/go/__init__.py
+++ waldo/go/__init__.py
@@ -1,1 +1,10 @@
-from .go import is_cellular_component
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009-2012 Luis Pedro Coelho <luis@luispedro.org>
+# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
+# License: MIT. See COPYING.MIT file in the Waldo distribution
+
+from .go import \
+        vocabulary, \
+        is_cellular_component, \
+        id_to_term, \
+        term_to_id
--- waldo/mgi/__init__.py
+++ waldo/mgi/__init__.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009-2012 Luis Pedro Coelho <luis@luispedro.org>
+# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
+# License: MIT. See COPYING.MIT file in the Waldo distribution
+
+from .retrieve import \
+        from_ensembl_gene_id, \
+        from_ensembl_peptide_id, \
+        retrieve_go_annotations, \
+        gen_url
+
 name = 'MGI'
