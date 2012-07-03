0a81f47fc3a339987097bb7ca4fd415ffcfdc8fb
Luis Pedro Coelho <luis@luispedro.org>
2012-07-03 16:53:10 +0100

    MIN Remove more Django stuff

--- woof/urls.py
+++ woof/urls.py
@@ -9,13 +9,6 @@ print settings._BASE_DIR + '/media'
 print 'urls'
 
 urlpatterns = patterns('',
-    (r'^media/(?P<path>.+)$', 'django.views.static.serve', {'document_root': settings._BASE_DIR + '/media'}),
     (r'^search/', include('woof.odlaw.urls')),
     (r'^json/', include('woof.odlaw.urls')),
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^admin/', include(admin.site.urls)),
-    (r'^help$', direct_to_template, {'template' : 'static/help.html'}, 'help'),
-    (r'^about$', direct_to_template, {'template' : 'static/about.html'}, 'about'),
-    (r'^contact-us$', direct_to_template, {'template' : 'static/contact-us.html'}, 'contact-us'),
-    (r'^$', direct_to_template, {'template' : 'index.html'}, 'home'),
 )
