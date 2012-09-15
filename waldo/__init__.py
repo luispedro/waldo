5e7be5963f871a85ff16f3d0740f8f5a2d5771cf
Luis Pedro Coelho <luis@luispedro.org>
2012-09-15 10:17:24 +0100

    DOC A little bit of info in the docstring

--- waldo/__init__.py
+++ waldo/__init__.py
@@ -1,1 +1,11 @@
+'''
+WALDO : WHERE PROTEINS ARE
+
+This is a Python library to collect information about proteins from online
+databases (Uniprot, MGI, ...) and expose it as an easy to use Python library.
+'''
 from .translations.services import translate
+
+__all__ = [
+    'translate',
+    ]
