15f4e071cc25824624058b36533dadb90a52ebc8
Luis Pedro Coelho <luis@luispedro.org>
2013-01-07 17:37:28 +0000

    MIN Export related in waldo.go

--- waldo/go/__init__.py
+++ waldo/go/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2009-2012 Luis Pedro Coelho <luis@luispedro.org>
+# Copyright (C) 2009-2013 Luis Pedro Coelho <luis@luispedro.org>
 # vim: set ts=4 sts=4 sw=4 expandtab smartindent:
 # License: MIT. See COPYING.MIT file in the Waldo distribution
 
@@ -7,4 +7,5 @@ from .go import \
         vocabulary, \
         is_cellular_component, \
         id_to_term, \
+        related, \
         term_to_id
